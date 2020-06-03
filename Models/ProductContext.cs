9ad28c28fb2627b9777b489fda5fdd0504b26a87
unknown <koffi20002006@gmail.com>
2020-06-03 13:45:17 +0200

    first commit

---
+++ Models/ProductContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WingtipToys.Models
+{
+    public class ProductContext : DbContext
+    {
+
+        public ProductContext() : base("WintipToys")
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
